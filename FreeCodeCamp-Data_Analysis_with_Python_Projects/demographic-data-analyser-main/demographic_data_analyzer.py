78dc57525b2eac6182734a9dac221f613f62e095
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-08-07 02:42:46 +0100

    feat: Solve education-richness aspect

--- FreeCodeCamp-Data_Analysis_with_Python_Projects/demographic-data-analyser-main/demographic_data_analyzer.py
+++ FreeCodeCamp-Data_Analysis_with_Python_Projects/demographic-data-analyser-main/demographic_data_analyzer.py
@@ -1,57 +1,34 @@
 import pandas as pd
 
-"""
-
-In this challenge you must analyse demographic data using Pandas. You are given a dataset of demographic data that was extracted from the 1994 Census database. Here is a sample of what the data looks like:
-
-|    |   age | workclass        |   fnlwgt | education   |   education-num | marital-status     | occupation        | relationship   | race   | sex    |   capital-gain |   capital-loss |   hours-per-week | native-country   | salary   |
-|---:|------:|:-----------------|---------:|:------------|----------------:|:-------------------|:------------------|:---------------|:-------|:-------|---------------:|---------------:|-----------------:|:-----------------|:---------|
-|  0 |    39 | State-gov        |    77516 | Bachelors   |              13 | Never-married      | Adm-clerical      | Not-in-family  | White  | Male   |           2174 |              0 |               40 | United-States    | <=50K    |
-|  1 |    50 | Self-emp-not-inc |    83311 | Bachelors   |              13 | Married-civ-spouse | Exec-managerial   | Husband        | White  | Male   |              0 |              0 |               13 | United-States    | <=50K    |
-|  2 |    38 | Private          |   215646 | HS-grad     |               9 | Divorced           | Handlers-cleaners | Not-in-family  | White  | Male   |              0 |              0 |               40 | United-States    | <=50K    |
-|  3 |    53 | Private          |   234721 | 11th        |               7 | Married-civ-spouse | Handlers-cleaners | Husband        | Black  | Male   |              0 |              0 |               40 | United-States    | <=50K    |
-|  4 |    28 | Private          |   338409 | Bachelors   |              13 | Married-civ-spouse | Prof-specialty    | Wife           | Black  | Female |              0 |              0 |               40 | Cuba             | <=50K    |
-
-
-You must use Pandas to answer the following questions:
-
--   How many people of each race are represented in this dataset? This should be a Pandas series with race names as the index labels. (`race` column)
--   What is the average age of men?
--   What is the percentage of people who have a Bachelor's degree?
--   What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
--   What percentage of people without advanced education make more than 50K?
--   What is the minimum number of hours a person works per week?
--   What percentage of the people who work the minimum number of hours per week have a salary of more than 50K?
--   What country has the highest percentage of people that earn >50K and what is that percentage?
--   Identify the most popular occupation for those who earn >50K in India.
-
-Use the starter code in the file `demographic_data_analyzer.py`. Update the code so all variables set to `None` are set to the appropriate calculation or code. Round all decimals to the nearest tenth.
-
-"""
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv("./adult.data.csv")
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    df["count"] = 1
+    race_count = df.groupby(["race"]).count()["count"]
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = round(df[df["sex"] == "Male"]["age"].mean(), 1) or round(df.loc[df["sex"] == "Male", "age"].mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = round(df["education"].value_counts(normalize=True)["Bachelors"] * 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    # high_mask = df[(df["education"] == "Bachelors") | (df["education"] == "Masters") | (df["education"] == "Doctorate")]["education"]
+    # higher_education = ((df["education"].value_counts(normalize=True)["Bachelors"] + df["education"].value_counts(normalize=True)["Masters"] + df["education"].value_counts(normalize=True)["Doctorate"]) * 100).round(1)
+
+    
+    higher_education = df[df["education"].isin(["Bachelors", "Masters", "Doctorate"])]
+    lower_education = df[~df["education"].isin(["Bachelors", "Masters", "Doctorate"])]
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = ((higher_education["salary"] == ">50K").value_counts(normalize=True) * 100).round(1)[True]
+    lower_education_rich = ((lower_education["salary"] == ">50K").value_counts(normalize=True) * 100).round(1)[True]
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
     min_work_hours = None
@@ -67,9 +44,6 @@ def calculate_demographic_data(print_data=True):
 
     # Identify the most popular occupation for those who earn >50K in India.
     top_IN_occupation = None
-
-
-
 
 
 
