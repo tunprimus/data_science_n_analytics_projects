9864926956cf67de22f052a9496a06adeb842700
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-09-12 16:00:56 +0100

    feat: Load, mark and normalise the DataFrame

--- FreeCodeCamp-Data_Analysis_with_Python_Projects/medical-data-visualiser-main/medical_data_visualizer.py
+++ FreeCodeCamp-Data_Analysis_with_Python_Projects/medical-data-visualiser-main/medical_data_visualizer.py
@@ -3,17 +3,43 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Monkey patching numpy >= 1.24 in order to successfully import from other libraries
+np.float = np.float64
+np.int = np.int_
+np.object = np.object_
+np.bool = np.bool_
+
+pd.set_option("mode.copy_on_write", True)
+
+RANDOM_SAMPLE_SIZE = 13
+
 # 1
 df = pd.read_csv("medical_examination.csv")
 df.head()
 df.info()
 df.describe()
-df.columns
+df_columns = df.columns
+df.sample(RANDOM_SAMPLE_SIZE)
 
 # 2
-df['overweight'] = None
+def mark_overweight(df):
+    return np.where(((df["weight"] / (df["height"] / 100) ** 2) > 25), 1, 0)
+
+
+df["overweight"] = df.apply(mark_overweight, axis=1)
+df.sample(RANDOM_SAMPLE_SIZE)
 
 # 3
+def data_normaliser(df):
+    return np.where((df <= 1), 0, 1)
+
+target_df = df.loc[:, ["cholesterol", "gluc", "smoke", "alco", "active", "cardio"]]
+buffer_df = df.loc[:, ["cholesterol", "gluc", "smoke", "alco", "active", "cardio"]].apply(data_normaliser)
+buffer_df.sample(RANDOM_SAMPLE_SIZE)
+print(buffer_df["cholesterol"].unique())
+
+df.loc[:, ["cholesterol", "gluc", "smoke", "alco", "active", "cardio"]] = df.loc[:, ["cholesterol", "gluc", "smoke", "alco", "active", "cardio"]].apply(data_normaliser)
+df.sample(RANDOM_SAMPLE_SIZE)
 
 
 # 4
@@ -21,21 +47,16 @@ def draw_cat_plot():
     # 5
     df_cat = None
 
-
     # 6
     df_cat = None
-    
 
     # 7
-
-
 
     # 8
     fig = None
 
-
     # 9
-    fig.savefig('catplot.png')
+    fig.savefig("catplot.png")
     return fig
 
 
@@ -50,15 +71,11 @@ def draw_heat_map():
     # 13
     mask = None
 
-
-
     # 14
     fig, ax = None
 
     # 15
 
-
-
     # 16
-    fig.savefig('heatmap.png')
+    fig.savefig("heatmap.png")
     return fig
