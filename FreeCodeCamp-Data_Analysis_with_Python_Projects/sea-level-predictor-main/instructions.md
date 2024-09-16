4eecdd06d429ccc8ec04d4a3d0845dd96ca7c210
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-09-16 04:35:22 +0100

    feat: Complete sea level predictor

---
+++ FreeCodeCamp-Data_Analysis_with_Python_Projects/sea-level-predictor-main/instructions.md
@@ -0,0 +1,29 @@
+# Sea Level Predictor
+
+
+## You will analyse a dataset of the global average sea level change since 1880. You will use the data to predict the sea level change through year 2050.
+
+
+
+## Tasks
+Use the data to complete the following tasks:
+- Use Pandas to import the data from `epa-sea-level.csv`.
+- Use matplotlib to create a scatter plot using the Year column as the x-axis and the `CSIRO Adjusted Sea Level` column as the y-axis.
+- Use the `linregress` function from `scipy.stats` to get the slope and y-intercept of the line of best fit. Plot the line of best fit over the top of the scatter plot. Make the line go through the year 2050 to predict the sea level rise in 2050.
+- Plot a new line of best fit just using the data from year 2000 through the most recent year in the dataset. Make the line also go through the year 2050 to predict the sea level rise in 2050 if the rate of rise continues as it has since the year 2000.
+- The x label should be `Year`, the y label should be `Sea Level (inches)`, and the title should be `Rise in Sea Level`.
+
+The boilerplate also includes commands to save and return the image.
+
+## Development
+
+Write your code in `sea_level_predictor.py`. For development, you can use `main.py` to test your code.
+
+
+## Testing
+
+The unit tests for this project are in `test_module.py`. We imported the tests from `test_module.py` to `main.py` for your convenience.
+
+## Submitting
+
+Copy your project's URL and submit it to freeCodeCamp.
--- FreeCodeCamp-Data_Analysis_with_Python_Projects/sea-level-predictor-main/sea_level_predictor.py
+++ FreeCodeCamp-Data_Analysis_with_Python_Projects/sea-level-predictor-main/sea_level_predictor.py
@@ -2,20 +2,48 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
+pd.set_option("mode.copy_on_write", True)
+
+RANDOM_SAMPLE_SIZE = 13
+GOLDEN_RATIO = 1.618
+FIGURE_WIDTH = 30
+FIGURE_HEIGHT = FIGURE_WIDTH / GOLDEN_RATIO
+
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv("epa-sea-level.csv")
+    df.head()
+    df.tail()
+    df.info()
+    df.describe()
+    df.sample(RANDOM_SAMPLE_SIZE)
 
     # Create scatter plot
+    fig, ax = plt.subplots(figsize=(FIGURE_WIDTH,FIGURE_HEIGHT))
+    ax.scatter(x="Year", y="CSIRO Adjusted Sea Level", data=df, color="blue", label="original data")
+    plt.title("Rise in Sea Level")
+    ax.set_xlabel("Year")
+    ax.set_ylabel("Sea Level (inches)")
+    plt.show()
 
 
     # Create first line of best fit
-
+    # Use the `linregress` function from `scipy.stats` to get the slope and y-intercept of the line of best fit. Plot the line of best fit over the top of the scatter plot. Make the line go through the year 2050 to predict the sea level rise in 2050.
+    slope, intercept, r_value, p_value, stderr = linregress(df["Year"], df["CSIRO Adjusted Sea Level"])
+    range_of_years = pd.Series(range(1880, 2050))
+    ax.plot(range_of_years, intercept + slope*range_of_years, color="red", label="first line of best fit")
+    fig
 
     # Create second line of best fit
-
+    # Plot a new line of best fit just using the data from year 2000 through the most recent year in the dataset. Make the line also go through the year 2050 to predict the sea level rise in 2050 if the rate of rise continues as it has since the year 2000.
+    df_2000 = df[df["Year"] >= 2000]
+    slope, intercept, r_value, p_value, stderr = linregress(df_2000["Year"], df_2000["CSIRO Adjusted Sea Level"])
+    range_of_years2 = pd.Series(range(2000, 2050))
+    ax.plot(range_of_years2, intercept + slope*range_of_years2, color="green", label="second line of best fit")
 
     # Add labels and title
+    ax.set(xlabel="Year", ylabel="Sea Level (inches)", title="Rise in Sea Level")
+    fig
 
     
     # Save plot and return data for testing (DO NOT MODIFY)
