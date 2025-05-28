84308c12a5298d62d0422752143a93c366e2325a
T Ol <31147296+tunprimus@users.noreply.github.com>
2025-05-28 13:11:16 +0100

    new file:   diagnostic-ultrasound-centre-project/src/utils/timing_function.py

--- diagnostic-ultrasound-centre-project/src/get_data/data_directories.py
+++ diagnostic-ultrasound-centre-project/src/get_data/data_directories.py
@@ -4,14 +4,16 @@ from os.path import expanduser, realpath
 # Define data directories
 raw_data_dir = realpath(expanduser("../../data/00-raw"))
 interim_data_dir = realpath(expanduser("../../data/01-interim"))
-processed_data_dir = realpath(expanduser("../../data/02-processed"))
+retraining_data_dir = realpath(expanduser("../../data/02-retraining"))
 final_data_dir = realpath(expanduser("../../data/03-final"))
 external_data_dir = realpath(expanduser("../../data/external"))
+database_dir = realpath(expanduser("../../data/database"))
 
 data_directories = {
     "raw_data_dir": raw_data_dir,
     "interim_data_dir": interim_data_dir,
-    "processed_data_dir": processed_data_dir,
+    "retraining_data_dir": retraining_data_dir,
     "final_data_dir": final_data_dir,
     "external_data_dir": external_data_dir,
+    "database_dir": database_dir,
 }
