66285d7d70fd1c292abd791d24806a200187e0a9
T Ol <31147296+tunprimus@users.noreply.github.com>
2025-11-13 12:01:52 +0100

    feat: Add time transformers

---
+++ diagnostic-ultrasound-centre-project/src/utils/time_transformers.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import numpy as np
+try:
+    import fireducks.pandas as pd
+except ImportError:
+    import pandas as pd
+    pd.set_option("mode.copy_on_write", True)
+from sklearn.preprocessing import FunctionTransformer, SplineTransformer
+
+
+def sin_transformer(period):
+    return FunctionTransformer(lambda x: np.sin(x / period * 2 * np.pi))
+
+
+def cos_transformer(period):
+    return FunctionTransformer(lambda x: np.cos(x / period * 2 * np.pi))
+
+
+def periodic_spline_transformer(period, n_splines=None, degree=3):
+    if n_splines is None:
+        n_splines = period
+    n_knots = n_splines + 1
+    return SplineTransformer(
+        degree=degree,
+        n_knots=n_knots,
+        knots=np.linspace(0, period, n_knots).reshape(n_knots, 1),
+        extrapolation="periodic",
+        include_bias=True,
+    )
+
+
